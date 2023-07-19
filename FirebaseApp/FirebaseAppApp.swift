134fdbc9b78c2b1dbc665739e35e5882f8815ed7
Nicolas <colacer11@gmail.com>
2023-07-19 18:41:54 -0500

    primercommit

---
+++ FirebaseApp/FirebaseAppApp.swift
@@ -0,0 +1,16 @@
+//
+//  FirebaseAppApp.swift
+//  FirebaseApp
+//
+
+
+import SwiftUI
+
+@main
+struct FirebaseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
